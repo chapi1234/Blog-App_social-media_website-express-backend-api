f961431e36fc1f25a5134227757be06d9209d3c9
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-31 19:11:02 +0300

    Add save post schema

---
+++ model/SavedPost.js
@@ -0,0 +1,8 @@
+const SavedPostSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+    savedAt: { type: Date, default: Date.now }
+  });
+  
+  module.exports = mongoose.model("SavedPost", SavedPostSchema);
+  
