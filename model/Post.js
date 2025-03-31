ba7eafa52f5f74ac72f580945e518495ba9336de
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-31 19:09:49 +0300

    Add post schema

---
+++ model/Post.js
@@ -0,0 +1,15 @@
+const PostSchema = new mongoose.Schema({
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true },
+    media: [{ type: String }],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+    tags: [{ type: String }],
+    isDraft: { type: Boolean, default: false },
+    visibility: { type: String, enum: ["public", "private", "friends"], default: "public" },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+  
+  module.exports = mongoose.model("Post", PostSchema);
+  
