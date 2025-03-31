ec8dabd9fafb143b41411f1c170c0907bcf82969
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-31 19:11:56 +0300

    Add Report schema

---
+++ model/Report.js
@@ -0,0 +1,11 @@
+const ReportSchema = new mongoose.Schema({
+    reportedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
+    commentId: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
+    reason: { type: String, required: true },
+    status: { type: String, enum: ["pending", "resolved"], default: "pending" },
+    createdAt: { type: Date, default: Date.now }
+  });
+  
+  module.exports = mongoose.model("Report", ReportSchema);
+  
