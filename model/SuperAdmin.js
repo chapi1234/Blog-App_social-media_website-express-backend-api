7234b2545dd96be19bdaf96d231375bcdceebe34
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-04-05 23:15:29 +0300

    update superadmin model

--- model/SuperAdmin.js
+++ model/SuperAdmin.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const SuperAdminSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, trim: true },
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true }, // Hashed
+  password: { type: String, required: true },
+  role: { type: String, enum: ["superadmin"], required: true, default: "superadmin" },
   permissions: {
     type: [String],
     enum: ["manage_admins", "manage_users", "manage_posts", "manage_comments", "manage_settings", "view_analytics"],
