92b300582d91de99ffa433f7047b2aece3f77116
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-04-05 23:14:54 +0300

    update moderator model

--- model/Moderator.js
+++ model/Moderator.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const ModeratorSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, trim: true },
+  name: { type: String, required: true, unique: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["moderator"], required: true, default: "moderator" },
+  status: { type: String, enum: ["active", "inactive"], default: "active" },
   permissions: [{ type: String, enum: ["manage_posts", "manage_comments"] }],
   assignedCategories: [{ type: String }], 
   profilePicture: { type: String, default: "" },
