9c55734f2bc392550a12595c8e05066475dc49c3
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-04-08 21:33:31 +0300

    update

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const helmet = require('helmet')
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
 
-
-// load environment variables from .env file
 dotenv.config() 
 const port = process.env.PORT || 3000
 const mongodb = process.env.MONGODB;
@@ -15,18 +14,44 @@ const app = express()
 
 
 // Midleware 
-app.use(express.json()) // Parse JSON bodies (as sent by API clients)
+app.use(express.json())
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'))
+app.use(cookieParser())
+
+// import routes
+const authRoute = require('./routes/auth')
+const superadminRoute = require('./routes/superadmin')
+const userRoute = require('./routes/user')
+const moderatorRoute = require('./routes/moderator')
+const postRoute = require('./routes/post')
+const commentRoute = require('./routes/comment')
+const savedpostRoute = require('./routes/savedPost')
+const reportRoute = require('./routes/report')
+const categoryRoute = require('./routes/category')
+const hashtagRoute = require('./routes/hashtag')
 
  
-// Start the Server
+// Route Middlewares
+app.use('/api/auth', authRoute)
+app.use('/api/superadmin', superadminRoute)
+app.use('/api/user', userRoute)
+app.use('/api/moderator', moderatorRoute)
+app.use('/api/post', postRoute)
+app.use('/api/comment', commentRoute)
+app.use('/api/savedpost', savedpostRoute)
+app.use('/api/report', reportRoute)
+app.use('/api/category', categoryRoute)
+app.use('/api/hashtag', hashtagRoute)
+
+// Connect to Server
 app.listen(port, () => { 
   console.log(`Server is running on port ${port}`);
 });
 
-// Connect to MongoDB
+
+// Connect to the database
 mongoose.connect(mongodb)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
