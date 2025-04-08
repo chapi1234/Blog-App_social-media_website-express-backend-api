8c80fd412a62de3580a887f7a2cbb8fd94ad0996
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-04-08 21:28:01 +0300

    Add moderator route

---
+++ routes/moderator.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const moderatorController = require('../controller/moderatorController');
+
+const { verifyToken, verifyRole } = require('../middleware/verifyToken');
+
+router.get('/', verifyToken, verifyRole(["admin", "superadmin"]), moderatorController.getAllModerators);
+router.get('/active', verifyToken, verifyRole(["admin", "superadmin"]), moderatorController.getAllActiveModerators);
+router.get('/inactive', verifyToken, verifyRole(["admin", "superadmin"]), moderatorController.getAllInactiveModerators);
+router.get('/:id', verifyToken, verifyRole(["admin", "superadmin"]), moderatorController.getModeratorById);
+router.put('/:id', verifyToken, verifyRole(["admin", "superadmin"]), moderatorController.updateModerator);
+router.delete('/:id', verifyToken, verifyRole(["admin", "superadmin"]), moderatorController.deleteModerator);
+
+
+module.exports = router;
