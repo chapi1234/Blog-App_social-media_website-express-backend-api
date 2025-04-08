9ea7071b77301cf26c2be7e9f3dcf43f9c2c9af2
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-04-08 21:32:30 +0300

    Add notification model

---
+++ model/Notification.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const NotificationSchema = new mongoose.Schema({
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    type: { type: String, enum: ["like", "comment", "follow"], required: true },
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
+    isRead: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now }
+  });
+  
+  module.exports = mongoose.model("Notification", NotificationSchema);
+  
