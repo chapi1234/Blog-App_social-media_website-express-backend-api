4efc1e676a945cf09ce5ec46109e2aa4731f8778
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-04-09 17:34:35 +0300

    final commit

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const mongoose = require('mongoose')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
+const { Server } = require('socket.io')
+const http = require('http')
 
 dotenv.config() 
 const port = process.env.PORT || 3000
 const mongodb = process.env.MONGODB;
 
 const app = express()
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }
+});
 
 
 // Midleware 
@@ -31,8 +41,15 @@ const savedpostRoute = require('./routes/savedPost')
 const reportRoute = require('./routes/report')
 const categoryRoute = require('./routes/category')
 const hashtagRoute = require('./routes/hashtag')
+const messageRoute = require('./routes/message')
+const notificationRoute = require('./routes/notification')
 
- 
+// pas `io` to the routes
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 // Route Middlewares
 app.use('/api/auth', authRoute)
 app.use('/api/superadmin', superadminRoute)
@@ -44,9 +61,25 @@ app.use('/api/savedpost', savedpostRoute)
 app.use('/api/report', reportRoute)
 app.use('/api/category', categoryRoute)
 app.use('/api/hashtag', hashtagRoute)
+app.use('/api/message', messageRoute)
+app.use('/api/notification', notificationRoute)
+
+// Socket.IO connection
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+  socket.on('sendMessage', (data) => {
+      console.log('Message received:', data);
+      io.emit('receiveMessage', data);
+  });
+
+  socket.on('disconnect', () => {
+      console.log('A user disconnected:', socket.id);
+  });
+});
+
 
 // Connect to Server
-app.listen(port, () => { 
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
