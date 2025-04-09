ecb00f4b831e55ce3955d0715aee8820f7b5d435
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-04-09 17:35:07 +0300

    update Notification model

--- model/Notification.js
+++ model/Notification.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const NotificationSchema = new mongoose.Schema({
     recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    type: { type: String, enum: ["like", "comment", "follow"], required: true },
+    type: { type: String, enum: ["like", "comment", "follow", "message"], required: true },
     postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
     isRead: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now }
